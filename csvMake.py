daccc0c39b85ae3d6c44137128a4c17c674b54ef
slaxit32 <dilushait@gmail.com>
2017-04-22 13:08:41 +0530

    normalize added

--- csvMake.py
+++ csvMake.py
@@ -1,13 +1,15 @@
 import csv
 import datetime
+import mumpy
 
 exampleFile = open('t1.csv')
 exampleReader = csv.reader(exampleFile)
 exampleData = list(exampleReader)
 
 
-	
+def normalize(v):
 
+   return(list(numpy.asarray(v) - min(v)))	
 
 def countFive(list,no):
 
@@ -41,44 +43,54 @@ def timeF():
 
 	return(lis2)
 
+def interp():
 
-def interp(li):
+	lis=[]
 
-	a=li
+	for i in range (len(exampleData)):
+		lis.append(exampleData[i][0])
 
-	t=float(a[-1]-a[0])/len(a)
+	a=lis
+
+
+	rang=int(a[-1])-int(a[0])
+	le=len(a)
+	
+	fact=float(rang)/le
+	print(fact)
 
 	b=[]
 	b.append(a[0])
 	for i in range (len(a)):
-		b.append(int(round(b[i]+t)))
+		b.append(int(round(int(b[i])+fact)))
 
 	return b
-
-
 
 def timeWrite():
 	lis=[]
 	for i in range (len(exampleData)):
 		#lis.append(int(int(exampleData[i][0])/1000))
 
-		print(datetime.datetime.fromtimestamp(int(exampleData[i][0])/1000).strftime('%Y-%m-%d %H:%M:%S'))
+		lis.append(datetime.datetime.fromtimestamp(int(exampleData[i][0])/1000).strftime('%Y-%m-%d %H:%M:%S'))
 	return(lis)
 
-
-
-
-listTime=timeF()
-listTimeSplitDecide=(countFive(listTime,5))
-
-
-
-
-############################# adding time range to example data
 def timeRange(val):
 
 	indexTimeVal=listTimeSplitDecide.index(val)+1
 	return(listTimeSplitDecide[indexTimeVal])
+
+
+	
+
+listTime=timeF()
+listTimeSplitDecide=(countFive(listTime,5))
+timeWriteOut=timeWrite()
+interPol=interp()
+std()
+
+
+############################# adding time range to example data
+
 
 lstTimeRange=[]
 for i in range (len(exampleData)):
@@ -94,7 +106,7 @@ outputWriter = csv.writer(outputFile)
 
 
 for i in range (len(exampleData)):
-	outputWriter.writerow([exampleData[i][0], exampleData[i][1],exampleData[i][2], exampleData[i][3],listTime[i],lstTimeRange[i]])
+	outputWriter.writerow([exampleData[i][0], exampleData[i][1],exampleData[i][2], exampleData[i][3],listTime[i],lstTimeRange[i],timeWriteOut[i],interPol[i]])
 
 
 #------write the time to file end -------------------------
